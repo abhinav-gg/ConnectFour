e889eb02c0c52cd4a4f9366ec5d81e111c6ade90
Abhinav <109210075+abhinav-gg@users.noreply.github.com>
2024-06-14 19:06:42 +0100

    Adding Render port

--- server/api.ts
+++ server/api.ts
@@ -5,11 +5,17 @@ import expressWs from 'express-ws';
 const API_ROUTE = '/api/v0/ws';
 
 const { app } = expressWs(express());
+const port = process.env.PORT || 4000; // Render needs a port specification or it keeps resetting itself when looking for open ports
+
 // const srv = new ApiHandler();
 
 // app.ws(API_ROUTE, (ws, req) => {
 //   ws.on('message', srv.handleMessage.bind(srv, ws));
 //   ws.on('close', srv.handleClose.bind(srv, ws));
 // });
+
+app.get('/test', (req, res) => {
+  res.send('Hello World!')
+})
 
 export default app;
