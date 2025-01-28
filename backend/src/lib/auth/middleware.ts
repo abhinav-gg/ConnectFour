0cfcea3bd8832a4df68832564339b7e77e71ec19
Astrochamp <56736275+Astrochamp@users.noreply.github.com>
2025-01-28 02:25:03 +0000

    add ichack font and remove logs

--- backend/src/lib/auth/middleware.ts
+++ backend/src/lib/auth/middleware.ts
@@ -151,7 +151,6 @@ export const verifyRecaptcha = async (req: RequestWithRecaptcha, res: Response, 
         success: false,
         message: 'reCAPTCHA verification failed'
       });
-      console.log('Secret:', secret, 'Token:', token); // debug
       return;
     }
 
@@ -173,7 +172,6 @@ export const verifyRecaptcha = async (req: RequestWithRecaptcha, res: Response, 
         success: false,
         message: 'reCAPTCHA verification failed',
       });
-      console.log('reCAPTCHA verification failed:', apiResponse); // debug
       return;
     }
 
