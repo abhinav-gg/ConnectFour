2d701803b59725547955163aeddc6f8f47faec49
Abhinav <109210075+abhinav-gg@users.noreply.github.com>
2025-02-01 15:56:36 +0000

    Update page.tsx (#61)

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -21,7 +21,7 @@ function LoginPage() {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const handleReCaptcha = useReCaptcha('login')
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     if (loading) return
