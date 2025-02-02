ee1fc53531ebf71ad2a6b402cc1941d7ba03920f
con4-app <connect-four@outlook.com>
2025-02-02 04:33:34 +0000

    People's lives just change (#63)
    I simplified them, whether they liked it or not :)
    
    Co-authored-by: Abhinav <agupta.cam7@gmail.com>

--- shared/constants.ts
+++ shared/constants.ts
@@ -2,12 +2,8 @@
 export const FriendlyNoEventGameMode = 'cd3eac8a-4a65-425e-8e0e-fda476f0d2b5';
 
 export const StandardTimecontrols = [
-    { id: 'rapid', label: 'Rapid', base: 10, increment: 0, disadvantage: 30 },
     { id: 'rapid2', label: 'Rapid', base: 5, increment: 2, disadvantage: 25 },
-    { id: 'blitz', label: 'Blitz', base: 5, increment: 0, disadvantage: 20 },
-    { id: 'blitz2', label: 'Blitz', base: 3, increment: 2, disadvantage: 15 },
-    { id: 'bullet2', label: 'Bullet', base: 2, increment: 0, disadvantage: 10 },
-    { id: 'bullet3', label: 'Bullet', base: 1, increment: 1, disadvantage: 5 },
+    { id: 'blitz2', label: 'Blitz', base: 3, increment: 2, disadvantage: 15 }
 ];
 
 export const StandardGameStates = {
